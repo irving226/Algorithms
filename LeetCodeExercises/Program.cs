aa43c4e13ab800a65e1974f6f4f51b8103b1ea43
Brian Irving <brianirv@buffalo.edu>
2021-03-01 21:36:38 -0700

    More Mods

--- LeetCodeExercises/Program.cs
+++ LeetCodeExercises/Program.cs
@@ -7,28 +7,12 @@ namespace LeetCodeExercises
         //Broken Calculator 
         static void Main(string[] args)
         {
-            int x = 5;
-            int y = 8;
-            int i = 0;
 
-            while (x != y)
-            {
-
-                if (x < y)
-                {
-                    x *= 2;
-                    i++;
-                   
-                }
-                else
-                {
-                    x -= 1;
-                    i++;
-                    Console.WriteLine($"x is now {x}");
-                }
-                Console.WriteLine("count=" + i);
-            }
 
         }
+
+        
     }
+   
 }
+
