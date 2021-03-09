512e981997e9e909881cbe7bdb989ab12540b985
Brian Irving <brianirv@buffalo.edu>
2021-03-08 20:09:30 -0700

    Added a new algorithm (RichestCust) and edited others

---
+++ RichestCustomerWealth/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RichestCustomerWealth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] accounts = new int[2][];
+
+            accounts[0] = new int[3] { 1, 2, 3 };
+            accounts[1] = new int [3] { 3, 2, 1 };
+
+            MaximumWealth(accounts);
+
+        }
+
+        public static int MaximumWealth(int[][] accounts)
+        {
+            int counter = 0;
+            int savedCounter=0;
+
+           for(int i = 0; i < accounts.Length; i++)
+            {
+                counter = 0;
+                foreach (int nums in accounts[i])
+                {
+                    counter += nums;
+                }
+                if (counter > savedCounter)
+                {
+                    savedCounter= counter;
+                }
+
+
+            }
+
+
+            return savedCounter;
+        }
+    }
+}
+
--- ShuffleArray/Program.cs
+++ ShuffleArray/Program.cs
@@ -5,11 +5,41 @@ namespace ShuffleArray
     class Program
     {
 
-        public int[] x = new int[] { 2, 5, 1, 3, 4, 7 };
         
         static void Main(string[] args)
         {
-            x.Length();
+        int[] nums = new int[] { 2, 5, 1, 3, 4, 7 };
+        int n = 3;
+        Shuffle(nums, n);
+        }
+
+        public static int[] Shuffle(int[] nums, int n)
+        {
+            //first take the array and cut it in half
+            //find a way to have those variables held(aray1. array)
+            //new array will be filled by each of the contents of the array one by one
+            // for each in one, add one from the other..
+
+            int[] arrayOne = new int[n];
+            int x = 0;
+            for(int i=n; i < nums.Length; i++)
+            {
+                arrayOne[x] = nums[i];
+                x++;
+            }
+            int[] arrayTwo = new int[nums.Length];
+
+            int y = 0;
+            for(int counter=0; counter < arrayTwo.Length; counter++)
+            {
+                arrayTwo[counter++] = nums[y];
+                arrayTwo[counter] = arrayOne[y];
+                
+               
+                y++;
+            }
+            return arrayTwo;
+            
         }
     }
 }
